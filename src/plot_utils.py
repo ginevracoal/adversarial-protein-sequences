ce2ce61d334aaf487cca86e1192c864c27faae58
Ginevra Carbone <ginevracoal@gmail.com>
2022-03-10 19:05:53 +0100

    small change on token choice

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 
 def plot_tokens_attention(scores_mat, sequence):
@@ -27,10 +28,10 @@ def plot_tokens_attention(scores_mat, sequence):
     return fig
 
 
-def plot_representations_norms(norms_mat, sequence):
+def plot_representations_norms(norms_mat, sequence, chosen_idx):
     n_layers = norms_mat.shape[0]
 
-    fig, ax = plt.subplots(figsize=(8,5))
+    fig, ax = plt.subplots(figsize=(10,7))
     ax = sns.heatmap(norms_mat, linewidth=0.2)
 
     ax.set_xticks(range(len(sequence)))
@@ -40,8 +41,22 @@ def plot_representations_norms(norms_mat, sequence):
     ax.set_xticklabels(sequence, fontdict=fontdict)
     ax.set_yticklabels(list(range(n_layers)), fontdict=fontdict, rotation=90)
 
+    x, y, w, h = chosen_idx, 0, 1, n_layers
+    ax.add_patch(Rectangle((x, y), w, h, fill=False, edgecolor='black', lw=2, clip_on=False))
+    ax.tick_params(length=0)
+
     plt.xlabel('Tokens')
     plt.ylabel('Layers')
+
+    plt.tight_layout()
     plt.show()  
 
+    return fig
+
+def plot_contact_maps(original_contacts, adversarial_contacts):
+
+    fig, ax = plt.subplots(figsize=(10, 6), ncols=2)
+    ax[0].imshow(original_contacts, cmap="Blues")
+    ax[1].imshow(adversarial_contacts, cmap="Blues")
+    plt.show()
     return fig
--- src/toy_example.py
+++ src/toy_example.py
@@ -2,11 +2,9 @@ import os
 import esm
 import torch
 import matplotlib.pyplot as plt
-# from captum.attr import Saliency, configure_interpretable_embedding_layer
 
-from embedding_model import EmbModel#, SaliencyWrapper
-from plot_utils import plot_tokens_attention, plot_representations_norms
-
+from embedding_model import EmbModel
+from plot_utils import plot_tokens_attention, plot_representations_norms, plot_contact_maps
 
 esm1_model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
 batch_converter = alphabet.get_batch_converter()
@@ -27,7 +25,7 @@ print("\nattentions.shape = (batch x layers x heads x seq_len x seq_len) =", res
 print("\nrepresentations.shape = (batch, seq_len, hidden_size) =", results['representations'][0].shape)
 print("\nlogits.shape = (batch, seq_len, n_layers) =", results['logits'].shape)
 
-# eval EmbModel on first embedding and check output logits are equal
+### eval EmbModel on first embedding and check output logits are equal
 
 first_embedding = results["representations"][0]
 
@@ -36,7 +34,7 @@ with torch.no_grad():
 
 assert torch.all(torch.eq(model(first_embedding)['logits'], results['logits']))
 
-# plot attention scores
+### plot attention scores
 
 last_layer_attention_scores = results['attentions'][:,n_layers-1,:,:,:].squeeze().detach().cpu().numpy()
 
@@ -47,18 +45,28 @@ fig = plot_tokens_attention(scores_mat=last_layer_attention_scores, sequence=ori
 fig.savefig(path+f"tokens_attention_layer={n_layers}.png")
 plt.close()
 
-# choose token that maximizes the norm of representations in the last layer
+### choose target token 
 
 representations = torch.stack(list(results["representations"].values())).squeeze()
-repr_norms = torch.norm(representations, dim=2)
+significant_tokens_repr = representations[:, 1:-1, :]
 
-fig = plot_representations_norms(norms_mat=repr_norms.cpu().detach().numpy(), sequence=original_sequence)
+# compute l2 norm of feature vectors
+repr_norms = torch.norm(significant_tokens_repr, dim=2, p=2) 
+
+# divide rows by max values (i.e. in each layer)
+repr_norms = torch.nn.functional.normalize(repr_norms, p=2, dim=1)
+
+# choose token idx that maximizes the sum of normalized scores in all layers
+chosen_token_idx = repr_norms.sum(dim=0).argmax().item()
+
+print("\nchosen_token_idx =", chosen_token_idx)
+
+fig = plot_representations_norms(norms_mat=repr_norms.cpu().detach().numpy(), 
+    sequence=original_sequence, chosen_idx=chosen_token_idx)
 fig.savefig(path+f"representations_norms.png")
 plt.close()
 
-chosen_token_idx = repr_norms[32].argmax().item()
-
-# attack first embedding on the chosen token
+### attack first embedding on the chosen token
 
 epsilon = 0.1
 
@@ -71,15 +79,15 @@ loss.backward()
 
 perturbed_embedding = first_embedding + epsilon * first_embedding.grad.data.sign()
 
-print(perturbed_embedding.shape)
+# print(perturbed_embedding.shape)
 print("\ndistance from the original embedding =", torch.norm(perturbed_embedding-first_embedding).item())
 
 first_embedding.requires_grad=False
 
-# find nearest hidden representation of perturbed sequences in the embedding space
+### find nearest hidden representation of perturbed sequences in the embedding space
 
 current_token = str(list(original_sequence)[chosen_token_idx])
-print("\ncurrent_token =", current_token)
+print(f"\ncurrent_token at position {chosen_token_idx} =", current_token)
 tokens_list = list(set(alphabet.standard_toks) - set(['.','-',current_token]))
 print("\nother tokens:", tokens_list)
 
@@ -98,11 +106,25 @@ token_representations = results["representations"][0]
 distances = torch.stack([torch.norm(perturbed_embedding-z_2) for z_2 in token_representations])
 min_dist_idx = torch.argmin(distances)
 
-print("\nnew token =", tokens_list[min_dist_idx])
+print(f"\nnew token at position {chosen_token_idx} =", tokens_list[min_dist_idx])
 print("\nadversarial sequence =", perturbed_data[min_dist_idx])
 print("\ndistance between embeddings =", distances[min_dist_idx].item())
 
+### compute contact maps
 
+batch_labels, batch_strs, batch_tokens = batch_converter(data)
+original_contacts = esm1_model.predict_contacts(batch_tokens)
+
+batch_labels, batch_strs, batch_tokens = batch_converter([perturbed_data[min_dist_idx]])
+adversarial_contacts = esm1_model.predict_contacts(batch_tokens)
+
+# euclidean_dist = torch.cdist(original_contacts, adversarial_contacts)**2
+# print("\neuclidean distance =", euclidean_dist)
+
+fig = plot_contact_maps(original_contacts=original_contacts[0].detach().numpy(), 
+    adversarial_contacts=adversarial_contacts[0].detach().numpy())
+fig.savefig(path+f"contact_maps.png")
+plt.close()
 
 
 
