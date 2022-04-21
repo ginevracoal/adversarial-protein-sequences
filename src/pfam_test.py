correct target token computation + target attention option

src/pfam_test.py
@@ -21,11 +21,13 @@ torch.manual_seed(0)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", default='fastaPF00004', type=str, help="Dataset name")
-parser.add_argument("--loss", default='maxTokensRepr', type=str, help="Dataset name")
+parser.add_argument("--loss", default='maxTokensRepr', type=str, help="Loss function")
+parser.add_argument("--target_attention", default='last_layer', type=str, help="Attention matrices used to \
+    choose target token idxs. Set to 'last_layer' or 'all_layers'.")
 parser.add_argument("--max_tokens", default=200, type=eval, help="Cut sequences to max number of tokens")
 parser.add_argument("--n_sequences", default=100, type=eval, help="Number of sequences from the chosen dataset. \
     None loads all sequences")
-parser.add_argument("--n_substitutions", default=10, type=int, help="Number of token substitutions in the original sequence")
+parser.add_argument("--n_substitutions", default=3, type=int, help="Number of token substitutions in the original sequence")
 parser.add_argument("--cmap_dist_lbound", default=100, type=int, help='Lower bound for upper triangular matrix of long \
     range contacts')
 parser.add_argument("--cmap_dist_ubound", default=20, type=int, help='Upper bound for upper triangular matrix of long \
@@ -89,7 +91,8 @@ else:
         atk = SequenceAttack(original_model=esm1_model, embedding_model=model, alphabet=alphabet)
 
         target_token_idxs, repr_norms_matrix = atk.choose_target_token_idxs(batch_tokens=batch_tokens, 
-            n_token_substitutions=args.n_substitutions, verbose=args.verbose)
+            n_token_substitutions=args.n_substitutions, target_attention=args.target_attention, 
+            verbose=args.verbose)
 
         signed_gradient, loss = atk.compute_loss_gradient(original_sequence=original_sequence, 
             target_token_idxs=target_token_idxs, first_embedding=first_embedding, loss=args.loss)

src/sequence_attack.py
@@ -17,29 +17,44 @@ class SequenceAttack():
         self.start, self.end = 4, 29 
         self.residues_tokens = self.alphabet.all_toks[self.start:self.end]
 
-    def choose_target_token_idxs(self, batch_tokens, n_token_substitutions, verbose=False):
+    def choose_target_token_idxs(self, batch_tokens, n_token_substitutions, target_attention, verbose=False):
 
         n_layers = self.original_model.args.layers
 
-        with torch.no_grad():
-            results = self.original_model(batch_tokens, repr_layers=list(range(n_layers)))
+        if target_attention=='all_layers':
+            layers_idxs = list(range(n_layers))
 
-        representations = torch.stack(list(results["representations"].values())).squeeze()
-        significant_tokens_repr = representations[:, 1:-1, :]
+        elif target_attention=='last_layer':
+            layers_idxs = [n_layers-1]
+
+        else:
+            raise AttributeError("Wrong target attention.")
+
+        with torch.no_grad():
+            results = self.original_model(batch_tokens, repr_layers=layers_idxs, return_contacts=True)
+
+        attentions = results["attentions"]
+        assert attentions.shape[0]==1 # check single input sequence
+
+        avg_heads_attentions = attentions.squeeze().mean(1)
+        significant_tokens_attentions = avg_heads_attentions[:, 1:-1, 1:-1]
+        assert significant_tokens_attentions.shape[0] == n_layers
+        assert significant_tokens_attentions.shape[1] == significant_tokens_attentions.shape[2] # = seq len
 
         # compute l2 norm of feature vectors
-        repr_norms = torch.norm(significant_tokens_repr, dim=2, p=2) 
+        attention_matrix = torch.norm(significant_tokens_attentions, dim=1, p=2)
+        # attention_matrix = torch.norm(significant_tokens_attentions, dim=2, p=2)
 
         # divide rows by max values (i.e. in each layer)
-        repr_norms_matrix = torch.nn.functional.normalize(repr_norms, p=2, dim=1)
-        
+        normalized_attention_matrix = torch.nn.functional.normalize(attention_matrix, dim=1, p=2)
+
         # choose top n_token_substitutions token idxs that maximize the sum of normalized scores in all layers
-        target_token_idxs = torch.topk(repr_norms_matrix.sum(dim=0), k=n_token_substitutions).indices.cpu().detach().numpy()
+        target_token_idxs = torch.topk(normalized_attention_matrix.sum(dim=0), k=n_token_substitutions).indices.cpu().detach().numpy()
 
         if verbose:
             print(f"\ntarget_token_idxs = {target_token_idxs}")
 
-        return target_token_idxs, repr_norms_matrix
+        return target_token_idxs, normalized_attention_matrix
 
     def compute_loss_gradient(self, original_sequence, target_token_idxs, first_embedding, loss, verbose=False):
 