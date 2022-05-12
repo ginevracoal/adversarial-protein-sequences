b89a3e448e84abe2ee709654610f8fb58757d287
Ginevra Carbone <ginevracoal@gmail.com>
2022-05-12 15:05:03 +0200

    added msa option

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,28 +1,59 @@
 import os 
+from Bio import Seq
 import pickle as pkl
+from Bio import SeqIO
+from Bio.Align import MultipleSeqAlignment
 
 
-def filter_pfam(max_tokens, filepath, filename):
+def load_pfam(filepath, filename, max_model_tokens, max_tokens=None, align=True):
 
     path = os.path.join(filepath, filename)
-    file = open(path, 'r')
+    min_seq_length = min([len(record.seq) for record in SeqIO.parse(path, format='fasta')])
 
-    file_contents = file.read().split('>')[0:-1]
+    if align:
 
-    avg_seq_lenght = 0
+        if max_tokens is None:
+            max_tokens = min_seq_length
 
-    data = []
-    for row in file_contents:
-        if row!='':
-            name, sequence = row.split('\n')[0:-1]
-            name = name.split('.1')[0]
-            sequence = sequence[:max_tokens]
+        data = []
+        for record in SeqIO.parse(path, format='fasta'):
+            record.seq = Seq.Seq(record.seq[:max_tokens])
+            data.append(record)
+
+        aligned_data = MultipleSeqAlignment(data)
+
+        data = []
+        for record in aligned_data:
+            name = record.name #.split('.')[0]
+            sequence = str(record.seq)
             data.append((name, sequence))
-            avg_seq_lenght += len(sequence)
-            assert len(sequence)<=max_tokens
 
-    avg_seq_lenght = int(avg_seq_lenght/len(data))
-    return data, avg_seq_lenght
+        print(f"\nSequences aligned and cut to {max_tokens} tokens")
+
+    else:
+
+        if max_tokens is None:
+            max_tokens = max_model_tokens
+
+        file = open(path, 'r')
+        file_contents = file.read().split('>')[0:-1]
+
+        data = []
+        avg_seq_lenght = 0
+        for row in file_contents:
+            if row!='':
+                name, sequence = row.split('\n')[0:-1]
+                # name = name.split('.')[0]
+                sequence = sequence[:max_tokens]
+                data.append((name, sequence))
+                avg_seq_lenght += len(sequence)
+                assert len(sequence)<=max_tokens
+
+        avg_seq_lenght = int(avg_seq_lenght/len(data))
+
+        print(f"\nSequences cut to {max_tokens} tokens")
+
+    return data
 
 
 ############
