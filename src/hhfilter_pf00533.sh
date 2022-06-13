2a89350675513fcb1fb1615753f1a96a8af21456
Ginevra Carbone <ginevracoal@gmail.com>
2022-06-13 18:48:15 +0200

    missense hhfilter + debugged toy example

---
+++ src/hhfilter_pf00533.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+### args
+
+DATASET="PF00533"
+FILTER_SIZE=100
+
+### set paths
+
+IN_FILENAME="/scratch/external/gcarbone/msa/seqs${DATASET}"
+OUT_PATH="/scratch/external/gcarbone/missense/hhfiltered/hhfiltered_${DATASET}_filter=${FILTER_SIZE}/"
+
+mkdir -p $OUT_PATH
+module load conda/4.9.2
+conda activate esm
+
+### split names and sequences
+
+cat $IN_FILENAME | awk 'NR % 2 == 1' > "${OUT_PATH}names"
+cat $IN_FILENAME | awk 'NR % 2 == 0' > "${OUT_PATH}full_sequences"
+
+######################################
+# First sequence in missense dataset #
+######################################
+
+### select columns without gaps and build a filtered MSA of minimum size FILTER_SIZE
+
+sequence="TPEEFMLVYKFARKHHITLTNLITEETTHVVMKTDAEFVCERTLKYFLGIAGGKWVVSYFWVTQSI"
+sequence_filename="P38398_1658-1723"
+OUT_NO_GAPS="${DATASET}_${sequence_filename}_no_gaps"
+cat "${OUT_PATH}full_sequences" > "${OUT_PATH}tmp"
+
+### remove all columns where the sequence has gaps 
+
+char_count=1
+for char in $(sed 's/./&\n/g' <(printf "$sequence")); do
+	
+	if [ "$char" = "-" ]; then
+
+		cat "${OUT_PATH}tmp" | cut --complement -c$char_count > "${OUT_PATH}swp"
+		cat "${OUT_PATH}swp" > "${OUT_PATH}tmp"
+		char_count=$((char_count-1))
+	fi
+
+	char_count=$((char_count+1))
+
+done
+
+# printf "\n"
+# cat "${OUT_PATH}tmp"
+
+### switch back to fasta type
+
+row_count=1
+
+for row_seq_name in $(cat "${OUT_PATH}names"); do
+
+	echo "$row_seq_name" >> $OUT_PATH$OUT_NO_GAPS
+	sed -n ${row_count}p "${OUT_PATH}tmp" >> $OUT_PATH$OUT_NO_GAPS
+
+	row_count=$((row_count+1))
+
+done
+
+# printf "\n"
+# cat $OUT_PATH$OUT_NO_GAPS
+
+### select top FILTER_SIZE seqs in the new msa
+
+OUT_NO_GAPS_FILTERED="${DATASET}_${sequence_filename}_no_gaps_filter=${FILTER_SIZE}"
+
+hhfilter -diff $FILTER_SIZE -i $OUT_PATH$OUT_NO_GAPS -o $OUT_PATH$OUT_NO_GAPS_FILTERED
+
+seq_count=$((seq_count+1))
+
+### delete temporary files
+
+rm "${OUT_PATH}tmp"
+rm "${OUT_PATH}swp"
+rm $OUT_PATH$OUT_NO_GAPS
+
+#######################################
+# Second sequence in missense dataset #
+#######################################
+
+sequence="PTDQL---EWMVQLCGASVSFTLGTGVHPIVVVQPD-AWTEDNGFHAIGQMCEAPVVTREWVLDS-"
+sequence_filename="P38398_1757-1841"
+OUT_NO_GAPS="${DATASET}_${sequence_filename}_no_gaps"
+cat "${OUT_PATH}full_sequences" > "${OUT_PATH}tmp"
+
+### remove all columns where the sequence has gaps 
+
+char_count=1
+for char in $(sed 's/./&\n/g' <(printf "$sequence")); do
+
+	if [ "$char" = "-" ]; then
+
+		cat "${OUT_PATH}tmp" | cut --complement -c$char_count > "${OUT_PATH}swp"
+		cat "${OUT_PATH}swp" > "${OUT_PATH}tmp"
+		char_count=$((char_count-1))
+	fi
+
+	char_count=$((char_count+1))
+
+done
+
+### switch back to fasta type
+
+row_count=1
+
+for row_seq_name in $(cat "${OUT_PATH}names"); do
+
+	echo "$row_seq_name" >> $OUT_PATH$OUT_NO_GAPS
+	sed -n ${row_count}p "${OUT_PATH}tmp" >> $OUT_PATH$OUT_NO_GAPS
+
+	row_count=$((row_count+1))
+
+done
+
+### select top FILTER_SIZE seqs in the new msa
+
+OUT_NO_GAPS_FILTERED="${DATASET}_${sequence_filename}_no_gaps_filter=${FILTER_SIZE}"
+
+hhfilter -diff $FILTER_SIZE -i $OUT_PATH$OUT_NO_GAPS -o $OUT_PATH$OUT_NO_GAPS_FILTERED
+
+seq_count=$((seq_count+1))
+
+### delete temporary files
+
+rm "${OUT_PATH}tmp"
+rm "${OUT_PATH}swp"
+rm $OUT_PATH$OUT_NO_GAPS
+
+rm "${OUT_PATH}names"
+rm "${OUT_PATH}full_sequences"
---
+++ src/toy_example.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python 
+
+import os
+import esm
+import torch
+import matplotlib.pyplot as plt
+
+from utils.data import *
+from utils.plot import *
+from utils.protein import compute_blosum_distance, get_contact_map
+from models.esm_embedding import EsmEmbedding
+from sequence_attack import SequenceAttack
+
+out_plots_path = '/fast/external/gcarbone/adversarial-protein-sequences_out/toy_example/'
+
+esm1_model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
+batch_converter = alphabet.get_batch_converter()
+n_layers = esm1_model.args.layers
+
+data = [
+    ("original_sequence", "MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG"),
+]
+batch_labels, batch_strs, batch_tokens = batch_converter(data)
+
+original_sequence = data[0][1]
+
+with torch.no_grad():
+    results = esm1_model(batch_tokens, repr_layers=list(range(n_layers)), return_contacts=True)
+
+print("\nresults.keys() =", results.keys())
+print("\nattentions.shape = (batch x layers x heads x seq_len x seq_len) =", results['attentions'].shape)
+print("\nrepresentations.shape = (batch, seq_len, hidden_size) =", results['representations'][0].shape)
+print("\nlogits.shape = (batch, seq_len, n_tokens) =", results['logits'].shape)
+
+first_embedding = results["representations"][0]
+
+# instantiate models
+
+model = EsmEmbedding(esm1_model, alphabet)
+
+atk = SequenceAttack(original_model=esm1_model, embedding_model=model, alphabet=alphabet)
+
+# attention scores
+
+plot_attention_grid(sequence=original_sequence, attentions=results['attentions'], layer_idx=n_layers, 
+    filepath=out_plots_path, filename=f"tokens_attention_layer={n_layers}")
+
+# target token idx
+
+target_token_idxs, target_tokens_attention = atk.choose_target_token_idxs(batch_tokens=batch_tokens, 
+    n_token_substitutions=3, target_attention='all_layers')
+print("\ntarget_token_idxs =", target_token_idxs)
+
+# attack
+
+signed_gradient, loss = atk.compute_loss_gradient(original_sequence=original_sequence, 
+            target_token_idxs=target_token_idxs, first_embedding=first_embedding, loss_method='max_tokens_repr')
+
+perturbations_keys = ['masked_pred','max_cos','min_dist','max_dist'] 
+
+atk_df, _ = atk.attack_sequence(name='seq', original_sequence=original_sequence, original_batch_tokens=batch_tokens,
+    target_token_idxs=target_token_idxs, target_tokens_attention=target_tokens_attention,
+    first_embedding=first_embedding, signed_gradient=signed_gradient, perturbations_keys=perturbations_keys, verbose=True)
+
+print("\n\natk_df.keys():", atk_df.keys())
+
+for key in perturbations_keys:
+    adversarial_sequence = atk_df[f'{key}_sequence'].unique()[0]
+    print(f"\n\n{key} sequence =", adversarial_sequence)
+
+    # blosum distance
+
+    blosum_distance = compute_blosum_distance(original_sequence, adversarial_sequence, target_token_idxs=target_token_idxs)
+    print("\nblosum_distance = ", blosum_distance)
+
+    # contact maps
+
+    original_contacts = get_contact_map(model=esm1_model, alphabet=alphabet, sequence=original_sequence)
+    adversarial_contacts = get_contact_map(model=esm1_model, alphabet=alphabet, sequence=adversarial_sequence)
+
+    distance = torch.norm((original_contacts-adversarial_contacts).flatten(), p=1).item()
+    print("\ndistance bw contact maps =", distance)
+
+    fig = plot_cmaps(original_contacts=original_contacts.detach().numpy(), 
+        adversarial_contacts=adversarial_contacts.detach().numpy(), 
+        filepath=out_plots_path, filename=f"{key}")
+
+
+
+
